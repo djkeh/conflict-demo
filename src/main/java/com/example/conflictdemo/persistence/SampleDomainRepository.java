fc5300248d0588f0077132487b2e580b0e12a4a2
Uno Kim <djkehh@gmail.com>
2019-12-24 03:33:08 +0900

    Add query method using fee

--- src/main/java/com/example/conflictdemo/persistence/SampleDomainRepository.java
+++ src/main/java/com/example/conflictdemo/persistence/SampleDomainRepository.java
@@ -3,9 +3,12 @@ package com.example.conflictdemo.persistence;
 import com.example.conflictdemo.domain.SampleDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 
 public interface SampleDomainRepository extends JpaRepository<SampleDomain, Long> {
     Optional<SampleDomain> findById(Long id);
+    List<SampleDomain> findByFee(BigDecimal fee);
 }
