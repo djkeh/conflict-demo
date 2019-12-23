cccd629af9f7d571b180b1416f647600d84d1c54
Uno Kim <djkehh@gmail.com>
2019-12-24 03:35:33 +0900

    I added fee on SampleDomain

--- src/main/java/com/example/conflictdemo/domain/SampleDomain.java
+++ src/main/java/com/example/conflictdemo/domain/SampleDomain.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.math.BigDecimal;
 
 
 @Entity
@@ -12,13 +13,16 @@ public class SampleDomain {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private BigDecimal fee;
     private String name;
     private int age;
 
+    public BigDecimal getFee() { return fee; }
     public Long getId() { return id; }
     public String getName() { return name; }
     public int getAge() { return age; }
 
+    public void setFee(BigDecimal fee) { this.fee = fee; }
     public void setName(String name) { this.name = name; }
     public void setAge(int age) { this.age = age; }
     public void setId(Long id) { this.id = id; }
